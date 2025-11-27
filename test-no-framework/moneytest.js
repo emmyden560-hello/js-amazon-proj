7f212198006c0334d1315dc43d3e0412541fc470
emmyd <emmanueldenis560@gmail.com>
2025-11-27 11:24:29 +0100

    rename test-jasmine into test n moved all test into one folder

---
+++ test-no-framework/moneytest.js
@@ -0,0 +1,8 @@
+import { currencyformat } from "../../scripts/utilities/money.js";
+
+if (currencyformat(2095) === '20.95') {
+    console.log('moneytest.js: PASSED');
+} else {
+    console.log('moneytest.js: FAILED');
+}
+
--- test/moneytest.js
+++
@@ -1,8 +0,0 @@
-import { currencyformat } from "../scripts/utilities/money.js";
-
-if (currencyformat(2095) === '20.95' ) {
-    console.log('moneytest.js: PASSED');
-} else {
-    console.log('moneytest.js: FAILED');
-}
-
