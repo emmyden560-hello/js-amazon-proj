55984027f3911c19c61cabe466a3c92e702347be
emmyd <emmanueldenis560@gmail.com>
2025-11-28 13:09:27 +0100

    learnt how to get requests with backend

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,6 +1,7 @@
 import { renderSummary } from "./checkout/order.js";
 import { paymentSummary } from "./checkout/paylog.js";
 // import "../data/cart-class.js";
+import '../data/backend-practice.js';
 
 renderSummary();
 paymentSummary();
