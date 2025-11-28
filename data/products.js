e71a45f70b13eeccc9ab1895ae5e7894a47700e3
emmyd <emmanueldenis560@gmail.com>
2025-11-28 11:50:00 +0100

    learn more tins abt classes n this

--- data/products.js
+++ data/products.js
@@ -52,6 +52,10 @@ class Clothing extends Products {
   }
 }
 
+const date = new Date();
+console.log(date);
+date.toLocaleTimeString();
+
 export const products = [{
   id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
   image: "images/products/athletic-cotton-socks-6-pairs.jpg",
