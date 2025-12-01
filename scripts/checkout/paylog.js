6b2dbd1f99e4ab1a4ce5e5881ecbfce4e94af6d4
emmyd <emmanueldenis560@gmail.com>
2025-12-01 10:53:00 +0100

    made place order interactive n used backend

--- scripts/checkout/paylog.js
+++ scripts/checkout/paylog.js
@@ -2,6 +2,7 @@ import { cart } from "../../data/cart.js";
 import { getProduct } from "../../data/products.js";
 import { getDeliveryOption } from "../../data/deliveryoptions.js";
 import { currencyformat } from "../utilities/money.js";
+import { addOrders } from "../../data/orders.js";
 
 export function paymentSummary() {
   let productPriceCents = 0;
@@ -49,11 +50,32 @@ export function paymentSummary() {
               <div class="payment-summary-money">$${currencyformat(totalCents)}</div>
             </div>
 
-            <button class="place-order-button button-primary">
+            <button class="place-order-button button-primary js-place-order">
               Place your order
             </button>
           `;
 
   document.querySelector('.js-payment').innerHTML = paymentSummaryHTML;
+
+  document.querySelector('.js-place-order').addEventListener('click', async () => {
+    try {
+      const response = await fetch('https://supersimplebackend.dev/orders', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          cart: cart
+        })
+      });
+
+      const order = await response.json();
+      addOrders(order);
+    } catch (error) {
+      console.log('Unexpected error try agn ltr')
+    }
+
+    window.location.href = 'orders.html'
+  });
 
 }
